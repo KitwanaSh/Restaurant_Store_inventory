eb9d5587c0809bb123a9783723010327b8d969c8
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-07-06 12:03:02 +0200

    Feat: Add context

---
+++ frontend/src/context/ContextProvider.jsx
@@ -0,0 +1,11 @@
+import React, { createContext, useState,useContext } from 'react'
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userPrfile:
+}
+
+export const contextProfiver
