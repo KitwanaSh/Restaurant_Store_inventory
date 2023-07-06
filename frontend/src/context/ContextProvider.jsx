246904339d797f69505d536295a85a6ad6ba3e4b
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-06 12:40:36 +0200

    Fix: Remove files

--- frontend/src/context/ContextProvider.jsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useState,useContext } from 'react'
-
-const StateContext = createContext();
-
-const initialState = {
-    chat: false,
-    cart: false,
-    userPrfile,
-}
