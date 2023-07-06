407ceb7873f3d6588f888451a122631474477e77
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-07-06 20:38:30 +0200

    Fix: Update the styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="mt-24">
-    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+  <div className="mt-8 mb-8 flex items-center justify-center">
+    <p className="dark:text-gray-200 text-gray-700 dark:bg-gray-900 text-center">
       © 2023 All rights reserved by Shoppy.com
     </p>
   </div>
