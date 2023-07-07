d67635f12ce99360aacda24365b8ea1f689bf45c
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-07-07 13:45:12 +0200

    Feat: Update the inventory dat

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -7,7 +7,7 @@ import { Header } from '../components';
 const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
-      <Header category = "Page" title = "Orders" />
+      <Header category = "Page" title = "Inventory" />
       <GridComponent id="gridComp" dataSource={ordersData} allowPaging allowSorting>
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
