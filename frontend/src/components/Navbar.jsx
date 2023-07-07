feac2edbcd2c8c753bb87d36f6d210150f798815
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-07-07 14:43:05 +0200

    Fix: Update the user profile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -77,7 +77,7 @@ const Navbar = () => {
             />
             <p>
               <span className='text-gray-400 text-14'>Hi, </span> {' '}
-              <span className='text-gray-400 font-bold ml-1 text-14'>Micheal</span>
+              <span className='text-gray-400 font-bold ml-1 text-14'>Ezechiel</span>
             </p>
             <MdKeyboardArrowDown className='text-gray-400 text-14' />
           </div>
