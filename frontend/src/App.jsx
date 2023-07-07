c1968ea4fabfe224a617f3cb3beca61df5539613
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-07-07 12:19:31 +0200

    Refractor: Remove unsed imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Kanban, Customers, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from "./pages";
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Kanban, Customers, Line, Bar, Pie, Financial, Editor } from "./pages";
 import { useStateContext } from './contexts/ContextProvider';
 
 import './App.css'
