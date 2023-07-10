f97aad18de736d135b4f225e3e2e773ade1e2a77
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-07-10 15:16:58 +0200

    Refractor: Update tha employee grid

--- frontend/src/data/dummy.jsx
+++ frontend/src/data/dummy.jsx
@@ -401,33 +401,40 @@ export const customersGrid = [
   { headerText: 'Name',
     width: '150',
     template: customerGridImage,
-    textAlign: 'Center' },
+    textAlign: 'Center'
+  },
   { field: 'ProjectName',
     headerText: 'Project Name',
     width: '150',
-    textAlign: 'Center' },
+    textAlign: 'Center'
+  },
   { field: 'Status',
     headerText: 'Status',
     width: '130',
     format: 'yMd',
     textAlign: 'Center',
-    template: customerGridStatus },
+    template: customerGridStatus
+  },
   {
     field: 'Weeks',
     headerText: 'Weeks',
     width: '100',
     format: 'C2',
-    textAlign: 'Center' },
+    textAlign: 'Center'
+  },
+
   { field: 'Budget',
     headerText: 'Budget',
     width: '100',
     format: 'yMd',
-    textAlign: 'Center' },
+    textAlign: 'Center'
+  },
 
   { field: 'Location',
     headerText: 'Location',
     width: '150',
-    textAlign: 'Center' },
+    textAlign: 'Center'
+  },
 
   { field: 'CustomerID',
     headerText: 'Customer ID',
