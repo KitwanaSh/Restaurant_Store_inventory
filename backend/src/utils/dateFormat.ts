594e29e410b699f5cdb08c8e08fbddefd008e0c5
hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-07-24 16:32:28 +0000

    update utils

--- backend/src/utils/dateFormat.ts
+++ backend/src/utils/dateFormat.ts
@@ -3,7 +3,3 @@ export const formatDate = (date: Date) => {
   const month = date.getUTCMonth();
   return `${day.toString().padStart(2, '0')}/${(month + 1).toString().padStart(2, '0')}`;
 }
-export const formatDate = (date: Date) => {	
-  const month = date.getUTCMonth();
-  return `${day.toString().padStart(2, '0')}/${(month + 1).toString().padStart(2, '0')}`;
-}
