934b8de46b38273aa1e9c1e83b9daa2cc54d3bc4
hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-07-24 16:31:49 +0000

    fix bug

--- backend/src/utils/dateFormat.ts
+++ backend/src/utils/dateFormat.ts
@@ -5,6 +5,5 @@ export const formatDate = (date: Date) => {
 }
 export const formatDate = (date: Date) => {	
   const month = date.getUTCMonth();
-  const day = date.getUTCMonth();
   return `${day.toString().padStart(2, '0')}/${(month + 1).toString().padStart(2, '0')}`;
 }
